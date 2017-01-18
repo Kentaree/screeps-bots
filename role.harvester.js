396b1770951b0fa25b9d7598bfba74170a16b158
Pieter Vos <kentaree@gmail.com>
2017-01-18 00:48:08 +0000

    Store energy in containers

--- role.harvester.js
+++ role.harvester.js
@@ -19,6 +19,7 @@ var roleHarvester = {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
                                 structure.structureType == STRUCTURE_SPAWN ||
                                 structure.structureType == STRUCTURE_STORAGE ||
+                                structure.structureType == STRUCTURE_CONTAINER ||
                                 structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
                     }
             });

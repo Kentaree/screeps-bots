a795df2c63e062bd0a9f4c9315b48a8227c99817
Pieter Vos <kentaree@gmail.com>
2017-01-19 23:06:55 +0000

    Prioritise spawners and extensions over other builds to drop off energy

--- role.harvester.js
+++ role.harvester.js
@@ -1,36 +1,54 @@
-let _ = require('lodash')
+let _ = require('lodash');
 
-var roleHarvester = {
+let roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            let closest;
-            if(creep.memory.source) {
-                closest = Game.getObjectById(creep.memory.source)
-            } else {
-                closest = creep.pos.findClosestByRange(FIND_SOURCES);
+        if(creep.memory.harvesting && creep.carry.energy == creep.carryCapacity) {
+            creep.say('Drop off');
+            creep.memory.harvesting = false;
+            let structures = creep.room.find(FIND_MY_STRUCTURES, { filter: (structure) => { return structure.structureType == STRUCTURE_SPAWN && structure.energy < structure.energyCapacity; } } )
+            if(!structures) {
+                structures = creep.room.find(FIND_MY_STRUCTURES, { filter: (structure) => { return structure.structureType == STRUCTURE_EXTENSION && structure.energy < structure.energyCapacity; } } )
             }
-            if(creep.harvest(closest) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(closest);
+            if(!structures) {
+                structures = creep.room.find(FIND_STRUCTURES, { filter: (structure) => { return ((structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity) || (structure.structureType == STRUCTURE_CONTAINER && _.sum(structure.store) < structure.storeCapacity) } } )
+            }
+            if(structures) {
+                let target = _.reduce(structures, function(result, structure) {
+                    let range=creep.pos.getRangeTo(structure);
+                    if(result && result.range < range) {
+                        return result;
+                    }
+                    return {range: range, structure: structure}
+                });
+                creep.memory.dropoff =  target.structure.id
             }
         }
-        else {
-            var closest = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return ((structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity) ||
-                            (structure.structureType == STRUCTURE_CONTAINER && _.sum(structure.store) < structure.storeCapacity);
-                    }
-            });
-            if(closest) {
-                creep.say('Moving to closest structure')
+
+        if(!creep.memory.harvesting && creep.carry.energy==0) {
+            creep.say('Harvesting');
+            creep.memory.harvesting = true;
+            creep.memory.source = creep.pos.findClosestByPath(FIND_SOURCES).id;
+        }
+
+        if(creep.memory.harvesting) {
+            if(creep.memory.source) {
+                closest = Game.getObjectById(creep.memory.source);
+                if(creep.harvest(closest) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(closest);
+                    creep.say('Move source');
+                }
+            }
+        }  else {
+            if(creep.memory.dropoff) {
+                let closest = Game.getObjectById(creep.memory.dropoff);
+                creep.say('Moving');
                 if(creep.transfer(closest, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(closest);
                 }
             } else {
-                creep.say('No structure close')
+                creep.say('No struct')
             }
         }
     }
